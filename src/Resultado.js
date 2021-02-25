69edf46474c10d1d539a2b3ef81bfaf44be70942
unknown <rodrigo.toblli@gmail.com>
2021-02-24 23:15:46 -0300

    css producto

--- src/Resultado.js
+++ src/Resultado.js
@@ -2,20 +2,23 @@ import producto from './Assets/producto.png'
 
 const Resultado = ()=>{
     return(
-        <article className='wrapper--main'>
-            <section className='wrapper--titulo--producto--imagen'>
-                <section className='wrapper--img'>
-                      <img className='producto--pic' src={producto}/>
+        <div>
+            <p className='categoria--producto'>Electrónica audio > video</p>
+            <article className='wrapper--main'>
+                <section className='wrapper--titulo--producto--imagen'>
+                    <section className='wrapper--img'>
+                        <img className='producto--pic' src={producto}/>
+                    </section>
+                    <article>
+                        <p>Nuevo 230 vendidos</p>
+                        <h4>Deco Reverse</h4>
+                        <h3>$1900</h3>
+                        <button>Comprar</button>
+                    </article>
                 </section>
-                <article>
-                    <p>Nuevo 230 vendidos</p>
-                    <h4>Deco Reverse</h4>
-                    <h3>$1900</h3>
-                    <button>Comprar</button>
-                </article>
-            </section>
 
-        </article>
+            </article>
+        </div>
     )
 }
 
