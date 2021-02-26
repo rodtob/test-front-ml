90be99590fdb9d9f88e36e733a70a398bfd8569f
rodtob <rodrigo.toblli@gmail.com>
2021-02-26 11:07:22 -0300

    llamados a la api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,46 @@
+import React,{useState, useEffect} from 'react'
 import Buscador from './Buscador'
 import Detalle from './Detalle';
 import Resultado from './Resultado';
 
-function App() {
+const App = ()=> {
+
+  const [data, setData] = useState(null);
+
+  const [id, setId] = useState('MLA709580097')
+
+  const [query, setQuery] = useState('pikachu')
+
+  useEffect(() => {
+
+      async function apiCall() {
+        let response = await fetch(`/api/items/${id}`)
+        response = await response.json()
+        setData(response)
+        console.log(response)
+      }
+
+     apiCall()    
+
+  }, [id]);
+
+  useEffect(() => {
+
+    async function apiCall() {
+      let response = await fetch(`/api/itemsq=${query}`)
+      response = await response.json()
+      setData(response)
+      console.log(response)
+    }
+
+   apiCall()    
+
+}, [query]);
+
+
+
+
+
   return (
     <div className="App">
       <Buscador/>
