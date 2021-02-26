82ea380b29f3a0c4e483cdeab6cb676de1d0ddcc
rodtob <rodrigo.toblli@gmail.com>
2021-02-25 23:54:37 -0300

    agrego busqueda por id en formato pedido

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -15,8 +15,34 @@ module.exports ={
 
         const response = await fetch(`${baseurl}/items/${req.params.id}`)
 
-        const json = await response.json()
 
-        res.send(json)
+        const resjson = await response.json()
+
+        const descripcion__response = await fetch(`${baseurl}/items/${req.params.id}/description`)
+
+        const resjsonD = await descripcion__response.json()
+
+        const elFormato = {
+            author: {
+                name: String,
+                lastname: String
+            },
+            item: {
+                id: resjson.id,
+                title: resjson.title,
+                price: {
+                currency: resjson.currency_id,
+                amount: Math.round(resjson.price),
+                decimals: parseFloat((resjson.price - Math.floor(resjson.price)).toFixed(2)),
+                },
+                picture: resjson.pictures[0].url,
+                condition: resjson.condition,
+                free_shipping: resjson.shipping.free_shipping,
+                sold_quantity: resjson.sold_quantity,
+                description: resjsonD.plain_text
+            
+        }}
+           
+        res.send(elFormato)
     }
 }
